92833c81a2af7c0fda8490e55bb93d6a955f4f9b
codewithseth <codewithseth2026@gmail.com>
2026-02-15 15:45:45 +0700

    refactor: update menu display methods for consistency and add subject, teacher, student, and address management options

--- src/App.java
+++ src/App.java
@@ -34,7 +34,7 @@ public class App {
                     int uniMenu;
                     
                     do {
-                        displayMenu("University");
+                        displayEntityMenu("University");
                         uniMenu = scanner.nextInt();
 
                         switch (uniMenu) {
@@ -76,7 +76,7 @@ public class App {
                     int catMenu;
                     
                     do {
-                        displayMenu("Category");
+                        displayEntityMenu("Category");
                         catMenu = scanner.nextInt();
 
                         switch (catMenu) {
@@ -114,13 +114,87 @@ public class App {
                     System.out.println("---------------------");
                     System.out.println("Subject Management");
                     System.out.println("---------------------");
+                    
+                    int subjMenu;
+
+                    do {
+                        displayEntityMenu("Subject");
+                        subjMenu = scanner.nextInt();
+
+                        switch (subjMenu) {
+                            case 1:
+                                System.out.println("---------------------");
+                                System.out.println("Create Subject");
+                                System.out.println("---------------------");
+                                break;
+                            case 2:
+                                System.out.println("---------------------");
+                                System.out.println("View Subject");
+                                System.out.println("---------------------");
+                                break;
+                            case 3:
+                                System.out.println("---------------------");
+                                System.out.println("Update Subject");
+                                System.out.println("---------------------");
+                                break;
+                            case 4:
+                                System.out.println("---------------------");
+                                System.out.println("Delete Subject");
+                                System.out.println("---------------------");
+                                break;
+                            default:
+                                System.out.println("---------------------");
+                                System.out.println("Invalid Menu!");
+                                System.out.println("Please choose from 1 to 4");
+                                System.out.println("---------------------");
+                                break;
+                        }
+                    } while (subjMenu != 0);
+                    
                     break;
                 case 4:
                     System.out.println("---------------------");
                     System.out.println("Teacher Management");
                     System.out.println("---------------------");
+
+                    int teacherMenu;
+
+                    do {
+                        displayEntityMenu("Teacher");
+                        teacherMenu = scanner.nextInt();
+
+                        switch (teacherMenu) {
+                            case 1:
+                                System.out.println("---------------------");
+                                System.out.println("Create Teacher");
+                                System.out.println("---------------------");
+                                break;
+                            case 2:
+                                System.out.println("---------------------");
+                                System.out.println("View Teacher");
+                                System.out.println("---------------------");
+                                break;
+                            case 3:
+                                System.out.println("---------------------");
+                                System.out.println("Update Teacher");
+                                System.out.println("---------------------");
+                                break;
+                            case 4:
+                                System.out.println("---------------------");
+                                System.out.println("Delete Teacher");
+                                System.out.println("---------------------");
+                                break;
+                            default:
+                                System.out.println("---------------------");
+                                System.out.println("Invalid Menu!");
+                                System.out.println("Please choose from 1 to 4");
+                                System.out.println("---------------------");
+                                break;
+                        }
+                    } while (teacherMenu != 0);
+
                     break;
-                case 5:
+                /* case 5:
                     System.out.println("---------------------");
                     System.out.println("Student Management");
                     System.out.println("---------------------");
@@ -129,6 +203,90 @@ public class App {
                     System.out.println("---------------------");
                     System.out.println("Address Management");
                     System.out.println("---------------------");
+                    break; */
+                case 5:
+                    System.out.println("---------------------");
+                    System.out.println("Student Management");
+                    System.out.println("---------------------");
+
+                    int studentMenu;
+
+                    do {
+                        displayEntityMenu("Student");
+                        studentMenu = scanner.nextInt();
+
+                        switch (studentMenu) {
+                            case 1:
+                                System.out.println("---------------------");
+                                System.out.println("Create Student");
+                                System.out.println("---------------------");
+                                break;
+                            case 2:
+                                System.out.println("---------------------");
+                                System.out.println("View Student");
+                                System.out.println("---------------------");
+                                break;
+                            case 3:
+                                System.out.println("---------------------");
+                                System.out.println("Update Student");
+                                System.out.println("---------------------");
+                                break;
+                            case 4:
+                                System.out.println("---------------------");
+                                System.out.println("Delete Student");
+                                System.out.println("---------------------");
+                                break;
+                            default:
+                                System.out.println("---------------------");
+                                System.out.println("Invalid Menu!");
+                                System.out.println("Please choose from 1 to 4");
+                                System.out.println("---------------------");
+                                break;
+                        }
+                    } while (studentMenu != 0);
+
+                    break;
+                case 6:
+                    System.out.println("---------------------");
+                    System.out.println("Address Management");
+                    System.out.println("---------------------");
+
+                    int addressMenu;
+
+                    do {
+                        displayEntityMenu("Address");
+                        addressMenu = scanner.nextInt();
+
+                        switch (addressMenu) {
+                            case 1:
+                                System.out.println("---------------------");
+                                System.out.println("Create Address");
+                                System.out.println("---------------------");
+                                break;
+                            case 2:
+                                System.out.println("---------------------");
+                                System.out.println("View Address");
+                                System.out.println("---------------------");
+                                break;
+                            case 3:
+                                System.out.println("---------------------");
+                                System.out.println("Update Address");
+                                System.out.println("---------------------");
+                                break;
+                            case 4:
+                                System.out.println("---------------------");
+                                System.out.println("Delete Address");
+                                System.out.println("---------------------");
+                                break;
+                            default:
+                                System.out.println("---------------------");
+                                System.out.println("Invalid Menu!");
+                                System.out.println("Please choose from 1 to 4");
+                                System.out.println("---------------------");
+                                break;
+                        }
+                    } while (addressMenu != 0);
+
                     break;
                 default:
                     System.out.println("---------------------");
@@ -142,11 +300,11 @@ public class App {
         scanner.close();
     }
 
-    private static final void displayMenu(String menuName) {
-        System.out.println("1. Create " + menuName);
-        System.out.println("2. View " + menuName);
-        System.out.println("3. Update " + menuName);
-        System.out.println("4. Delete " + menuName);
+    private static void displayEntityMenu(String menuName) {
+        System.out.println(String.format("1. Create %s", menuName));
+        System.out.println(String.format("2. View %s", menuName));
+        System.out.println(String.format("3. Update %s", menuName));
+        System.out.println(String.format("4. Delete %s", menuName));
         System.out.println("0. Exit");
         System.out.print("Select Menu: ");
     }
--- src/constant/DBConstant.java
+++ src/constant/DBConstant.java
@@ -1,6 +1,9 @@
 package constant;
 
 public interface DBConstant {
+    /**
+     * implicitly public static final.
+     */
     String DB_URL = "jdbc:mysql://localhost/ums_db";
     String DB_USER = "usr";
     String DB_PASSWORD = "pwd";
